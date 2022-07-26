33e22642d9a82fd4213e3629937d8ca10c3a1c1f
Oliver Cederborg <hey@olivercederborg.com>
2022-07-26 03:23:38 +0200

    feat(nvim): update treesitter config

--- .config/nvim/lua/oliver/treesitter.lua
+++ .config/nvim/lua/oliver/treesitter.lua
@@ -12,6 +12,15 @@ configs.setup {
 		enable = true,
 	},
 
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { }, -- list of language that will be disabled
