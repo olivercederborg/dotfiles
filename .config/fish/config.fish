8fc183863664e3134982adad77f102f7dd83e91c
Oliver Cederborg <hey@olivercederborg.com>
2022-08-16 13:17:42 +0200

    feat(fish): add config

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+set -U fish_greeting "🐟"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# aliases
+alias vim='nvim'
+alias vi='nvim'
+alias r='ranger'
+alias ls='exa'
+alias dots='~/scripts/dots.sh'
+alias tim='~/bin/tim'
+
+# functions
+function cd..
+  cd ..
+end
+
+function sudo
+  if test "$argv" = !!
+    eval command sudo $history[1]
+  else
+    command sudo $argv
+  end
+end
