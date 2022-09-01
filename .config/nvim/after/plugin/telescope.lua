72f22e21391b406b6a9450be3da2d7ead8a95fcf
Oliver Cederborg <hey@olivercederborg.com>
2022-09-02 01:39:56 +0200

    feat(nvim): update plugin mappings

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,9 @@
-local present, telescope = pcall(require, 'telescope')
+local present, telescope = pcall(require, "telescope")
 if not present then
   return
 end
 
-local present_, actions = pcall(require, 'telescope.actions')
+local present_, actions = pcall(require, "telescope.actions")
 if not present_ then
   return
 end
@@ -12,59 +12,59 @@ end
 
 vim.g.theme_switcher_loaded = true
 
-telescope.setup {
+telescope.setup({
   defaults = {
     vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
     },
-    prompt_prefix = '   ',
-    selection_caret = '  ',
-    entry_prefix = '  ',
-    initial_mode = 'insert',
-    selection_strategy = 'reset',
-    sorting_strategy = 'ascending',
-    layout_strategy = 'horizontal',
+    prompt_prefix = "   ",
+    selection_caret = "  ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
-        prompt_position = 'top',
+        prompt_position = "top",
         preview_width = 0.5,
         results_width = 0.5,
         width = 0.7,
         height = 0.85,
       },
     },
-    file_sorter = require('telescope.sorters').get_fuzzy_file,
-    file_ignore_patterns = { 'node_modules', 'package-lock' },
-    generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-    path_display = { 'truncate' },
+    file_sorter = require("telescope.sorters").get_fuzzy_file,
+    file_ignore_patterns = { "node_modules", "package-lock" },
+    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+    path_display = { "truncate" },
     winblend = 0,
     border = true,
-    borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
     color_devicons = true,
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
 
     mappings = {
       i = {
-        ['<C-n>'] = actions.move_selection_next,
-        ['<C-e>'] = actions.move_selection_previous,
+        ["<C-k>"] = actions.move_selection_next,
+        ["<C-j>"] = actions.move_selection_previous,
       },
 
       n = {
-        ['q'] = actions.close,
+        ["q"] = actions.close,
 
-        ['n'] = actions.move_selection_next,
-        ['e'] = actions.move_selection_previous,
+        ["k"] = actions.move_selection_next,
+        ["j"] = actions.move_selection_previous,
 
-        ['gg'] = actions.move_to_top,
-        ['G'] = actions.move_to_bottom,
+        ["gg"] = actions.move_to_top,
+        ["G"] = actions.move_to_bottom,
       },
     },
   },
   pickers = {},
-}
+})
