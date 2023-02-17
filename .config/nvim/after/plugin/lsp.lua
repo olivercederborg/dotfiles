f59d34a9741eccbdede1539fcca2947412e0d2cd
Oliver Cederborg <hey@olivercederborg.com>
2023-02-17 23:50:42 +0100

    feat(nvim): update

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,197 @@
+function Contains(list, x)
+	for _, v in pairs(list) do
+		if v == x then
+			return true
+		end
+	end
+	return false
+end
+
+local lsp = require("lsp-zero")
+local lspkind = require("lspkind")
+
+local snip_present, luasnip = pcall(require, "luasnip")
+if not snip_present then
+	return
+end
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	"tsserver",
+	"lua_ls",
+	"rust_analyzer",
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+
+local cmp = require("cmp")
+
+lsp.setup_nvim_cmp({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	mapping = {
+		["<C-e>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		["<esc>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		-- Accept currently selected item. If none selected, `select` first item.
+		-- Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		}),
+	},
+	view = {
+		entries = "custom",
+	},
+	window = {
+		completion = {
+			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+			--[[ border = "none", ]]
+		},
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = strings[1] .. " "
+			kind.menu = "    " .. strings[2]
+
+			return kind
+		end,
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	},
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	},
+	experimental = {
+		ghost_text = false,
+	},
+})
+
+lsp.set_preferences({
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "",
+		warn = "",
+		hint = "",
+		info = "",
+	},
+})
+
+local disabled_client_formatters = {
+	"tsserver",
+}
+
+lsp.on_attach(function(client, bufnr)
+	-- Autoformat on save
+	--[[ local augroup = vim.api.nvim_create_augroup("LspFormatting", {}) ]]
+	--[[ if client.server_capabilities.documentformattingprovider then ]]
+	--[[ 	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr }) ]]
+	--[[ 	vim.api.nvim_create_autocmd("bufwritepre", { ]]
+	--[[ 		group = augroup, ]]
+	--[[ 		buffer = bufnr, ]]
+	--[[ 		callback = function() ]]
+	--[[ 			vim.lsp.buf.format({ bufnr = bufnr }) ]]
+	--[[ 		end, ]]
+	--[[ 	}) ]]
+	--[[ end ]]
+	local opts = { remap = false, buffer = bufnr }
+
+	vim.keymap.set("n", "gD", function()
+		vim.lsp.buf.declaration()
+	end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "gh", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "gi", function()
+		vim.lsp.buf.implementation()
+	end, opts)
+	vim.keymap.set("n", "<C-y>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+	vim.keymap.set("n", "<leader>r", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("n", "gr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>.", "<cmd>CodeActionMenu<CR>", opts)
+	vim.keymap.set("n", "gl", function()
+		vim.diagnostic.open_float({ border = "single" })
+	end, opts)
+	vim.keymap.set("n", "<C-q>", function()
+		vim.diagnostic.goto_prev({ border = "single" })
+	end, opts)
+	vim.keymap.set("n", "<C-a>", function()
+		vim.diagnostic.goto_next({ border = "single" })
+	end, opts)
+	vim.keymap.set("n", "<leader>q", function()
+		vim.diagnostic.setloclist()
+	end, opts)
+
+	vim.api.nvim_create_user_command("Format", function()
+		vim.lsp.buf.format({ async = false })
+	end, {})
+
+	-- Disable formatting capabilities for specific clients
+	if Contains(disabled_client_formatters, client.name) then
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+	end
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+	virtual_text = true,
+})
---
+++ .config/nvim/after/plugin/null-ls.lua
@@ -0,0 +1,43 @@
+local null_ls_present, null_ls = pcall(require, "null-ls")
+if not null_ls_present then
+	return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier,
+		formatting.gofmt,
+		formatting.stylua,
+		formatting.black,
+
+		diagnostics.eslint_d,
+		diagnostics.flake8,
+
+		code_actions.eslint_d,
+
+    require("typescript.extensions.null-ls.code-actions"),
+	},
+	-- Autoformat on save
+	on_attach = function(client, bufnr)
+		if client.server_capabilities.documentFormattingProvider then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end
+	end,
+})
