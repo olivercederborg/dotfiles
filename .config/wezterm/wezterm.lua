48a139390a81f02557a22d74abb9028ffcd34e3a
Oliver Cederborg <hey@olivercederborg.com>
2023-08-27 22:27:31 +0200

    feat(wezterm): update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,6 +13,11 @@ end)
 return {
 	default_cursor_style = "BlinkingBlock",
 	color_scheme = "Poimandres",
+	colors = {
+		cursor_bg = "#A6ACCD",
+		cursor_border = "#A6ACCD",
+		cursor_fg = "#1B1E28",
+	},
 	-- font
 	font = wezterm.font("JetBrains Mono", { weight = "Medium" }),
 	font_size = 15,
