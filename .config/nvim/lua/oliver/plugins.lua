97f77dadc41cbca7e7c9c4b0b333b6aa9416703d
Oliver Cederborg <hey@olivercederborg.com>
2023-09-04 01:40:14 +0200

    feat(nvim): update lazygit config

--- .config/nvim/lua/oliver/plugins.lua
+++ .config/nvim/lua/oliver/plugins.lua
@@ -233,7 +233,17 @@ return {
 
 	-- Git
 	{ "lewis6991/gitsigns.nvim" },
-	{ "kdheepak/lazygit.nvim" },
+	{
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			vim.g.lazygit_floating_window_scaling_factor = 0.85 -- scaling factor for floating window
+			vim.g.lazygit_floating_window_border_chars = { " ", " ", " ", " ", " ", " ", " ", " " } -- customize lazygit popup window border characters
+		end,
+	},
 
 	-- Copilot
 	{ "github/copilot.vim" },
