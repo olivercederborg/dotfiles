4f0815b9b3dd0d1a853c9128b5552374b9791e11
Oliver Cederborg <hey@olivercederborg.com>
2023-09-09 22:48:19 +0200

    feat(nvim): update config

--- .config/nvim/lua/oliver/autocommands.lua
+++ .config/nvim/lua/oliver/autocommands.lua
@@ -6,15 +6,3 @@ autocmd("TextYankPost", {
 	group = autogroup("YankHighlight", { clear = true }),
 	command = "silent! lua vim.highlight.on_yank()",
 })
-
--- Inlay hints
---[[ autocmd("LspAttach", { ]]
---[[ 	group = autogroup("UserLspConfig", {}), ]]
---[[ 	callback = function(args) ]]
---[[ 		local client = vim.lsp.get_client_by_id(args.data.client_id) ]]
---[[ 		if client.server_capabilities.inlayHintProvider then ]]
---[[ 			vim.lsp.inlay_hint(args.buf, true) ]]
---[[ 		end ]]
---[[ 		-- whatever other lsp config you want ]]
---[[ 	end, ]]
---[[ }) ]]
